97fceda63c9280cb5d7209f45699917d806fc448
Leeru <aslirubiz@gmail.com>
2025-10-05 23:31:48 -0500

    Add files via upload

--- silhouette.js
+++ silhouette.js
@@ -1,67 +1,69 @@
-var ImgList = [
-    "botella",
-    "concha",
-    "concha2",
-    "delfin",
-    "estrella",
-    "medusa",
-    "perla",
-    "pez"
-]
+var images = ["botella", "concha", "concha2", "delfin", "estrella", "medusa", "perla", "pez"];
 
-var ImgSet;
-var board = [];
 var rows = 2;
 var columns = 4;
 
 var selectedImg;
-var currentImg
+var currentImg;
+
 var time = 0;
+var score = 0;
 
 window.onload = function() {
-    startGame();
     randomImg();
+    startGame();
     //timer();
 
 }
 
-function randomImg() {
-    let random = document.createElement("img");
-    let randomnumber = Math.floor(Math.random() * ImgSet.length);
-    random = randomnumber;
-    document.getElementById("silueta").append(random);
+const randomImage = Math.floor(Math.random()* images.length);
 
-    random.src = ImgList.shift() + ".png";
+//Aparece una silueta al azar
+function randomImg() {   
+    const image = document.getElementById("image");
+    image.src = images[randomImage] + ".png";
 }
 
+//Aparecen todas las imágenes disponibles
 function startGame() {
     for (let r = 0; r < rows; r++){
+
+        let row = [];
+
         for (let c = 0; c < columns; c++) {
+
+            let pieceImg = images.pop();
+            row.push(pieceImg);
+            
             //<img>
             let tile = document.createElement("img");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.src = ImgList.shift() + ".png";
+            tile.src = pieceImg + ".png";
             document.getElementById("board").append(tile);
-            //tile.addEventListener("click", compare);
+            tile.addEventListener("click", compare);
             
         }
     }
 
 }
 
-//function compare() {
-//    if (selectedImg == currentImg){
-//        time += 1;
-//    }
-//    else{
+function compare() {
 
-//    }
-//}
+    currentImg;
+    selectedImg;
 
-//function timer() {
-//    time = document.querySelector("span");
-//}
+    if (selectedImg.src == currentImg.src){
+        score += 10;
+        let audioCorrecto = new Audio("");
+    }
+    else { 
+        let audioIncorrecto = new Audio("");
+    }
+}
 
-//function reiniciarJuego() {
-//    document.getElementById("restart");
-//}
+function timer() {
+    time = document.querySelector("span");
+}
+
+function reiniciar() {
+    document.getElementById("reiniciar");
+}
