6a1b1e8db538904a23648200f2286fea7acd3dae
Leeru <aslirubiz@gmail.com>
2025-10-22 20:01:24 -0500

    Add files via upload

--- silhouette.js
+++ silhouette.js
@@ -3,50 +3,52 @@ var images = ["botella", "concha", "concha2", "delfin", "estrella", "medusa", "p
 var rows = 2;
 var columns = 4;
 
-var addImg = 0;
 var selectedImg;
 var currentImg;
+var addImg = 0;
 
-var time = 0;
+var seconds = 30;
 var score = 0;
 
 window.onload = function() {
     randomImg();
     startGame();
-    //timer();
-    //reload();
+    timer();
 }
 
-//Aparece una silueta al azar
+//Cambia la silueta al azar
 function randomImg() {
     var image = document.getElementById("image");
     var randomImage = Math.floor(Math.random()* images.length);
     image.src = images[randomImage] + ".png";
-    console.log("Hiciste click a una de las imágenes");
 }
 
-//Aparecen todas las imágenes en el tablero
+//Agrega todas las imágenes en el tablero
 function startGame() {
+
     for (let r = 0; r < rows; r++){
         for (let c = 0; c < columns; c++) {
 
             let pieceImg = images[addImg];
             addImg ++;
 
-            //<img>
+            //<img id= 0-0>
             let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString(); //Esto es para saber qué imagen fue clickeada
             tile.src = pieceImg + ".png";
             document.getElementById("board").append(tile);
             tile.addEventListener("click", compare);
             
         }
     }
+
+    console.log("Juego iniciado correctamente");
 }
 
 // Se compara si la silueta y la imagen seleccionada son iguales
 function compare() {
 
-/*  if (selectedImg.src == currentImg.src){
+  /*if (selectedImg.src == currentImg.src){
         score += 10;
         document.getElementById("score").innerHTML = score;
         var audioCorrecto = new Audio("./beep-6-96243.mp3");
@@ -57,15 +59,32 @@ function compare() {
         audioIncorrecto.play();
     }
 
-    setTimeout(wait, 1000); */
+    setTimeout(wait, 1000);*/
 
     randomImg();
 }
 
+//Temporizador
 function timer() {
-    time = document.querySelector("span");
+    let countdown = document.getElementById("countdown");
+
+    if (seconds == 0) {
+        alert("Time out!");
+        return;
+    }
+    else {
+        countdown.innerHTML = seconds;
+        seconds--;
+        setTimeout(timer, 1000); //1000 or 1E3
+    }
+
+    if (seconds < 10 && seconds >= 0) {
+        countdown.innerHTML = "0" + seconds;
+    }
 }
 
 function reload() {
-    document.getElementById("reiniciar");
+    seconds = 30;
+    score = 0;
+    //document.querySelector(".score").textContent = "0";
 }
